a18a1531ca88776649b5cefc29d8607af151ae8a
kataklinger <kataklinger@gmail.com>
2020-09-29 23:58:57 +0200

    In-place construction of child chains

--- src/inc/forque.hpp
+++ src/inc/forque.hpp
@@ -108,17 +108,16 @@ namespace detail {
                   detail::rebind_alloc_t<allocator_type, sibling_type>>;
     using sibling_iter = typename sibling_list::iterator;
 
-    using next_ptr = std::shared_ptr<next_type>;
     using next_allocator_type =
         detail::rebind_alloc_t<allocator_type, next_type>;
 
     using children_map = std::unordered_map<
         next_key_type,
-        next_ptr,
+        next_type,
         std::hash<next_key_type>,
         std::equal_to<next_key_type>,
         detail::rebind_alloc_t<allocator_type,
-                               std::pair<const next_key_type, next_ptr>>>;
+                               std::pair<const next_key_type, next_type>>>;
 
     struct segment {
       inline bool forked() const noexcept {
@@ -227,12 +226,12 @@ namespace detail {
     task<reservation_type> reserve_child(View view, mutex_guard&& guard) {
       auto& child = ensure_child(view);
 
-      co_await child->mutex_.lock();
-      mutex_guard guard_child{child->mutex_, std::adopt_lock};
+      co_await child.mutex_.lock();
+      mutex_guard guard_child{child.mutex_, std::adopt_lock};
 
       mutex_guard{std::move(guard)};
 
-      co_return co_await child->reserve(view, std::move(guard_child));
+      co_return co_await child.reserve(view, std::move(guard_child));
     }
 
     template<viewlike View>
@@ -243,14 +242,10 @@ namespace detail {
         return it->second;
       }
 
-      auto child = std::allocate_shared<next_type>(
-          next_allocator_type{children.get_allocator()},
-          *sink_,
-          this,
-          view.sub());
-
       auto [pos, result] =
-          children.insert(std::make_pair(view.key(), std::move(child)));
+          children.emplace(std::piecewise_construct,
+                           std::forward_as_tuple(view.key()),
+                           std::forward_as_tuple(*sink_, this, view.sub()));
 
       return pos->second;
     }
@@ -320,7 +315,7 @@ namespace detail {
 
     task<> activate_children(segment_iter segment_pos) {
       for (auto& [_, child] : segment_pos->children_) {
-        co_await child->activate_segment();
+        co_await child.activate_segment();
       }
     }
 
@@ -354,10 +349,10 @@ namespace detail {
       if (child_pos != segment_pos->children_.end()) {
         auto& [child_key, child] = *child_pos;
 
-        co_await child->mutex_.lock();
-        mutex_guard guard_child{child->mutex_, std::adopt_lock};
+        co_await child.mutex_.lock();
+        mutex_guard guard_child{child.mutex_, std::adopt_lock};
 
-        if (child->get_version() == version) {
+        if (child.get_version() == version) {
           segment_pos->children_.erase(child_pos);
           if (segment_pos->children_.empty()) {
             mutex_guard{std::move(guard_child)};
